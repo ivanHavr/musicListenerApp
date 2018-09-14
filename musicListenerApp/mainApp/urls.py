3045a7ed472cb61eda671d100713b60ec17f935d
IvanHavr <henkok211@gmail.com>
2018-09-14 23:56:50 +0200

    change settings

--- musicListenerApp/mainApp/urls.py
+++ musicListenerApp/mainApp/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('login', index, name='index'),
     # path(r'^login/$', views.login, name='login'),
     # path(r'^logout/$', views.logout, name='logout'),
-    path(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
+    path(r'auth/', include('social_django.urls', namespace='social')),  # <- Here
     path(r'', home, name='home'),
 ]
